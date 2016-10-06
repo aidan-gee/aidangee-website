20241386bf94ad92ce3b38d2f696ce3be519c6c0
aidan-gee <aidangeewd@gmail.com>
2016-10-06 01:43:20 +0100

    First commit - set up with react create app and added a basic page

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'font-awesome/css/font-awesome.css';
+import './material-social-buttons.css';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
